559a0022521fbd502e97c64d5fdeb6512e090f00
effiecancode <faitheffie25@gmail.com>
2023-12-08 20:28:38 +0300

    all tests passed

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function addingEventListener() {
+    let input;
+    input = document.getElementById('button');
+    input.addEventListener('click', function() {
+        alert('clicked');
+    });
 }
-
